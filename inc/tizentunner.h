7d0cd4a99e1730aa85f6925d895f0f08ec046aae
seiwin92 <seiwin92@naver.com>
2015-10-02 13:04:43 +0900

    g
    hhhg

---
+++ inc/tizentunner.h
@@ -0,0 +1,19 @@
+#ifndef __tizentunner_H__
+#define __tizentunner_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "tizentunner"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.tizentunner"
+#endif
+
+#endif /* __tizentunner_H__ */
